6beb55e90514b03a0487551338ab4b44a478db6a
Allen John <allenjohnmonapallil@gmail.com>
2024-06-11 11:27:43 +0530

    port-update

--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const instance = axios.create({
-    baseURL: 'http://localhost:8000', 
+    baseURL: 'https://login-logout-jxyq.onrender.com', 
     headers: {
       'Content-Type': 'application/json', 
     },
